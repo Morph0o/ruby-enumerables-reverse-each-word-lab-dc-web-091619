9d9125af7aa4441b5d0f4b77127a973986aa601a
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-09-10 23:18:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 def reverse_each_word(string)
   new = []
   newstring = string.split()
-  newstring.each do|newstring| new.push(newstring.reverse())
+  newstring.collect do|newstring| new.push(newstring.reverse())
  end
 new.join(" ")
 end
