12aea6276e58353d4a11fd8f81f61ae636b3a9d3
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-09-10 23:09:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
+require "pry"
 def reverse_each_word(string)
   new = []
   newstring = string.split("")
+  binding.pry
   newstring.each do|newstring| new.push(newstring.reverse())
 end
 new.join("")
