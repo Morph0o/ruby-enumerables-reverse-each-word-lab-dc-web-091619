8fe51c8fd8571fd09503342a2b3e184cadeaadd3
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-09-10 22:48:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new = []
   string.split("")
-  string.each do|string| new.push(string.reverse())
+  string.each() do|string| new.push(string.reverse())
 end
 new.join("")
 end
